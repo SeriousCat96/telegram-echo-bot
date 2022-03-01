463ae92f11adcc77fc174fa438bc2999c3d0572d
seriouscat96 <thisisbusiness@yandex.ru>
2022-03-02 01:30:14 +0300

    fixed send message model

--- EchoBot/Models/SendMessageModel.cs
+++ EchoBot/Models/SendMessageModel.cs
@@ -4,7 +4,7 @@ namespace EchoBot.WebApp.Models
 {
 	public class SendMessageModel
 	{
-		public ChatId ChatId { get; set; }
+		public long ChatId { get; set; }
 		public string Message { get; set; }
 		public int? ReplyToMessageId { get; set; }
 	}
